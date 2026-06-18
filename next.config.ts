0e4754ec8623263cf2947f3cc2a61971729f4c1c
TradingMorning <falconspido.com@gmail.com>
2026-06-18 06:09:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Prevent Next.js from checking or creating routes from your custom pages folder */
+  useFileSystemPublicRoutes: false,
 };
 
 export default nextConfig;
+
+
+
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
